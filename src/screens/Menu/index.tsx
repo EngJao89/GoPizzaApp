b2cc762c00a56e56e5f05a0e05186fef055dca93
EngJao89 <eng_joao@outlook.com>
2024-08-05 23:27:53 -0300

    feat(gpa22-menu-list): implementa lista de pizzas em cardápio

---
+++ src/screens/Menu/index.tsx
@@ -0,0 +1,29 @@
+import {ImageSourcePropType, ScrollView} from 'react-native';
+
+import {Container} from './styles';
+import {ListHeader} from '../../components/ListHeader';
+import {CardMenu} from '../../components/CardMenu';
+
+import {MENU} from '../../data/menu';
+
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+  image: ImageSourcePropType;
+  description: string;
+};
+
+export function Menu() {
+  return (
+    <Container>
+      <ListHeader />
+
+      <ScrollView>
+        {MENU.map(item => (
+          <CardMenu key={item.id} {...item} />
+        ))}
+      </ScrollView>
+    </Container>
+  );
+}

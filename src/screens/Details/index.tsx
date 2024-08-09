fa03762223ce255cefd895eb76058f48e63bb3d0
EngJao89 <eng_joao@outlook.com>
2024-08-08 23:52:56 -0300

    feat(gpa25-info-details-content): implementa detalhes e titulo de seletor

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -1,11 +1,22 @@
 import {useNavigation} from '@react-navigation/native';
 import {useTheme} from 'styled-components/native';
 import {ArrowLeft} from 'phosphor-react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-import {BackContent, Container, Image, Subtitle, Title} from './styles';
+import {
+  BackContent,
+  Container,
+  ContentText,
+  Image,
+  Info,
+  SelectText,
+  Subtitle,
+  Title,
+} from './styles';
 import {IconButton} from '../../components/IconButton';
 
 import pizza from '../../assets/products/margherita.png';
+import Divider from '../../components/Divider';
 
 export function Details() {
   const {COLORS} = useTheme();
@@ -28,6 +39,21 @@ export function Details() {
       <Title>Margherita</Title>
 
       <Subtitle>Mussarela, manjericão fresco, parmesão e tomate.</Subtitle>
+
+      <Info>
+        <Icon name="stopwatch" color={COLORS.RED_1} size={24} />
+        <ContentText>30 Min</ContentText>
+        <Divider orientation="vertical" />
+        <Icon
+          name="fire-alt"
+          color={COLORS.RED_1}
+          size={24}
+          style={{marginLeft: 16}}
+        />
+        <ContentText>880 kcal</ContentText>
+      </Info>
+
+      <SelectText>Selecione um tamanho</SelectText>
     </Container>
   );
 }

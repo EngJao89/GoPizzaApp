19eff13b97bb632164020c932a11d5ad34813d5d
EngJao89 <eng_joao@outlook.com>
2024-08-13 23:49:49 -0300

    feat(gpa31-items-cart): implementa card de item selecionados do cardápio para tela de conta

---
+++ src/components/ItemCart/styles.ts
@@ -0,0 +1,54 @@
+import {styled} from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  padding: 16px;
+  flex-direction: row;
+  margin-top: 8px;
+  margin-left: 8px;
+  margin-right: 8px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  gap: 8px;
+`;
+
+export const Info = styled.View`
+  gap: 4px;
+`;
+
+export const Name = styled.Text`
+  color: ${({theme}) => theme.COLORS.GRAY_2};
+  font-size: ${({theme}) => theme.FONT_SIZE.LG};
+  font-weight: bold;
+`;
+
+export const Description = styled.Text`
+  color: ${({theme}) => theme.COLORS.GRAY_3};
+  font-size: ${({theme}) => theme.FONT_SIZE.XS};
+  font-weight: 400;
+`;
+
+export const Image = styled.Image`
+  margin-right: 4px;
+  height: 96px;
+  width: 96px;
+`;
+
+export const Price = styled.View`
+  flex-direction: row;
+  align-items: baseline;
+  gap: 4px;
+`;
+
+export const Currency = styled.Text`
+  color: ${({theme}) => theme.COLORS.GREEN_2};
+  font-size: ${({theme}) => theme.FONT_SIZE.LG};
+  font-weight: bold;
+`;
+
+export const Value = styled.Text`
+  color: ${({theme}) => theme.COLORS.GREEN_2};
+  font-size: ${({theme}) => theme.FONT_SIZE.LG};
+  font-weight: bold;
+`;
--- src/screens/Cart/index.tsx
+++ src/screens/Cart/index.tsx
@@ -1,10 +1,23 @@
 import {CartHeader} from '../../components/CartHeader';
-import {Container} from './styles';
+import Divider from '../../components/Divider';
+import {ItemCart} from '../../components/ItemCart';
+import {Container, Content, Label, TotalContent, Value} from './styles';
 
 export function Cart() {
   return (
     <Container>
       <CartHeader />
+      <Content>
+        <ItemCart />
+        <Divider orientation="horizontal" />
+        <ItemCart />
+        <Divider orientation="horizontal" />
+        <TotalContent>
+          <Label>Total da Conta</Label>
+          <Value>R$ 99,80</Value>
+        </TotalContent>
+        <Divider orientation="horizontal" />
+      </Content>
     </Container>
   );
 }
